56abaa2b2a29d431aa0e8fd1e469131983756b63
Mathis Borges <mathis.bc23@gmail.com>
2026-02-04 15:15:03 +0100

    add gitignore

--- services/user/dist/user.entity.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class User {
-    id: number;
-    username: string;
-    email: string;
-    password_hash: string;
-    avatar_url: string;
-}
--- services/user/dist/users-group.entity.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class UsersGroup {
-    id: number;
-    user_id: number;
-    group_id: number;
-    role: string;
-}
